435d0c626e91a5068af7d5e7ee74b8ee0a930a61
Shiyu Cheng <starkeyshiyu@gmail.com>
2019-08-10 02:56:02 -0700

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,7 +20,7 @@ module.exports = function (grunt) {
             dev:{
                 bsFiles:{
                     src: [
-                        'css/*.csss',
+                        'css/*.css',
                         '*.html',
                         'js/*.js'
                     ]
@@ -32,8 +32,71 @@ module.exports = function (grunt) {
                     }
                 }
             }
+        },
+        copy:{
+            html:{
+                files:[{
+                    expand: true,
+                    dot: true,
+                    cwd: './',
+                    src: ["*.html"],
+                    dest:'dist'
+
+                }]
+            },
+            fonts:{
+                files:[{
+                    expand: true,
+                    dot:true,
+                    cwd: 'node_modules/font-awesome',
+                    src: ['fonts/*.*'],
+                    dest:'dist'
+                }]
+            },
+        },
+        clean: {
+            build:{
+                src:['dist/']
+            }
+        },
+        imagemin:{
+            dynamic:{
+                files:[{
+                    expand:true,
+                    dot: true,
+                    cwd: './',
+                    src: ["img/*.{png, jpg, gif}"],
+                    dest: 'dist/'
+                }]
+            }
+        },
+        useminPrepare:{
+            foo:{
+                dest:'dist',
+                src: ['contactus.html', 'aboutus.html', 'index.html']
+            },
+            options:{
+                flow: {
+                    steps:{
+                        css:['cssmin'],
+                        js: ['uglify'],
+                    },
+                    post:{
+                        css: [{
+                            name: 'cssmin',
+                            createConfig:function (context, block) {
+                                var generated = context.options.generated;
+                                generated.options = {
+                                    keepSpecialComments: 0, rebase: false
+                                };
+                            }
+                        }]
+                    }
+                }
+            }
         }
     });
     grunt.registerTask('css', ['sass']);
     grunt.registerTask('default', ['browserSync', 'watch']);
+    grunt.registerTask('build', ['clean', "copy", 'imagemin']);
 };
